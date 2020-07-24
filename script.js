9f23711136adb9107f160487e18252a83d172b3d
daudisa <daudisa@boisestate.edu>
2020-07-24 10:47:54 -0600

    script

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 // window.onload = function sayHi(){ alert("Hello World of Javascript!");}
 
-
+function myFunction() {
+    document.getElementById("button").innerHTML = "Published: July 23, 2020";
+  }
