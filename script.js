180132de952f381ecb84f643ea53d8d3d819f12c
daudisa <daudisa@boisestate.edu>
2020-07-25 19:20:30 -0600

    script

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 // window.onload = function sayHi(){ alert("Hello World of Javascript!");}
 
-function myFunction() {
+function myButton() {
     document.getElementById("button").innerHTML = "Published: July 23, 2020";
   }
+
+  function myColor() {
+    document.getElementById("color").style.color = "gray";
+  }
+
+ function myAlert() {
+     alert("Submitted")
+ }
+
+
+
+ function getRandomNumber (number) {
+    number = math.random () * number;
+    newDiv.innerHTML = "Hi";
+    newDiv.style.color = "maroon"
+  }
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
